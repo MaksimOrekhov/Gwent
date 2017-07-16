7f23b72f6fd404e0abce746c5f0b6474da09a6ff
Maksim Orekhov <timberwolf.max@gmail.com>
2017-07-16 15:12:49 +0300

    second commit

--- gwent.js
+++ gwent.js
@@ -1,16 +1,17 @@
-$(document).ready( function() {  
+// $(document).ready( function() {  
 
-        var newPhoto = new Image();
-        var oldPhoto = $('#swap').attr('src');
-        newPhoto.src = '/Users/maksimo/Documents/JavaScript/cards-win.32.png';
+//         var newPhoto = new Image();
+//         var oldPhoto = $('#swap').attr('src');
+//         newPhoto.src = '/Users/maksimo/Documents/Gwent/JavaScript/cards-win.32.png';
 
-        $('#swap').hover(
-                function() {
-                    $(this).attr('src', newPhoto.src);
-                },
-                function() {
-                    $(this).attr('src', oldPhoto);
-                }
+//         $('#swap').hover(
+//                 function() {
+//                     $(this).attr('src', newPhoto.src);
+//                 },
+//                 function() {
+//                     $(this).attr('src', oldPhoto);
+//                 }
 
-            );  // end hover
-            }); // end ready
+//             );  // end hover
+//             }); // end ready
+
